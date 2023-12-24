2b1f6f405cff531494240ef56edf88fa444774d3
Changsu Park <gd12091@gmail.com>
2023-12-24 18:11:06 +0900

    FEAT : Admin page

--- Practice/Practice/urls.py
+++ Practice/Practice/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("csroot/", admin.site.urls),  # URL Reverse
     path("instagram/", include("instagram.urls")),
 ]
--- Practice/instagram/models.py
+++ Practice/instagram/models.py
@@ -3,5 +3,12 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Custom Post object ({self.id})"
+
+    def message_length(self):
+        return len(self.message)
