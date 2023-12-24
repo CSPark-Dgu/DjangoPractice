7601a44d14e09ae617f1ee2bcc885630fc9c75e2
Changsu Park <gd12091@gmail.com>
2023-12-24 17:30:58 +0900

    REFACTOR : Reformat by Black

--- Practice/Practice/urls.py
+++ Practice/Practice/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('instagram/', include('instagram.urls')),
+    path("admin/", admin.site.urls),
+    path("instagram/", include("instagram.urls")),
 ]
