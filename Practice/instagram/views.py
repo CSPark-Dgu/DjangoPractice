477024a8ff0fea416c60ed10c2f320af801d51e6
Changsu Park <gd12091@gmail.com>
2024-01-01 22:50:07 +0900

    CBV display views(ListView)

--- Practice/instagram/views.py
+++ Practice/instagram/views.py
@@ -4,23 +4,23 @@ from .models import Post
 from django.http import HttpResponse, Http404
 
 
-# post_list = ListView.as_view(model=Post) #CBV
+post_list = ListView.as_view(model=Post, paginate_by=10)  # CBV
 
 
-def post_list(request: HttpResponse) -> HttpResponse:
-    qs = Post.objects.all()
-    q = request.GET.get("q", "")
-    if q != "":
-        qs = qs.filter(message__icontains=q)
-    # instagram/templates/instagram/post_list.html
-    return render(
-        request,
-        "instagram/post_list.html",
-        {
-            "post_list": qs,
-            "q": q,
-        },
-    )
+# def post_list(request: HttpResponse) -> HttpResponse:
+#     qs = Post.objects.all()
+#     q = request.GET.get("q", "")
+#     if q != "":
+#         qs = qs.filter(message__icontains=q)
+#     # instagram/templates/instagram/post_list.html
+#     return render(
+#         request,
+#         "instagram/post_list.html",
+#         {
+#             "post_list": qs,
+#             "q": q,
+#         },
+#     )
 
 
 # def post_detail(request: HttpResponse, pk: int) -> HttpResponse:
